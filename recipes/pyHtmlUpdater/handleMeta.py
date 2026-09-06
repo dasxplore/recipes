ac481e398bc38aa5a421dc156d6e2637ec1a8437
Somnath Das <somnath.das@daslearning.in>
2026-09-06 17:20:44 +0530

    adding meta tag automation

--- recipes/pyHtmlUpdater/handleMeta.py
+++ recipes/pyHtmlUpdater/handleMeta.py
@@ -1,11 +1,18 @@
 import re
 import yaml
+import os
 from bs4 import BeautifulSoup
+from pathlib import Path
+
+# global vars
+base_path = Path(__file__).resolve().parent.parent
 
 FRONT_MATTER_RE = re.compile(
     r"^---[ \t]*\r?\n(.*?\r?\n)(?:---|\.\.\.)[ \t]*\r?\n",
     re.DOTALL
 )
+
+# Global functions
 
 def get_meta_tags(md_file:str) -> dict | None :
     front_matter = None
@@ -15,7 +22,6 @@ def get_meta_tags(md_file:str) -> dict | None :
     if match:
         front_matter = yaml.safe_load(match.group(1))
     return front_matter
-
 
 def get_file_paths(obj):
     paths = []
@@ -54,30 +60,48 @@ def set_meta_property(soup, prop, content):
 
     meta["content"] = content
 
-## Run it
 
-with open("../mkdocs.yml", "r") as f:
+## Run it ##
+
+metas = ["keywords"]
+meta_ogs = ["og:image", "og:title", "og:description", "og:url"]
+mkdocs_path = base_path / "mkdocs.yml"
+docs_path = base_path / "docs"
+site_path = base_path / "site"
+print(f"site_path: {site_path}")
+
+# get the nav details from mkdocs configuration
+with open(mkdocs_path, "r") as f:
     mkdocs = yaml.safe_load(f)
 
 paths = get_file_paths(mkdocs["nav"])
-print(paths)
 
-html_file = "index.html" # will be in loop
-with open(html_file, "r", encoding="utf-8") as f:
-    soup = BeautifulSoup(f, "html.parser")
+for path in paths:
+    md_path = docs_path / path
+    html_path = site_path / path
+    html_path = html_path.with_suffix(".html")
 
-set_meta(
-    soup,
-    "keywords",
-    "indian recipes, healthy recipe, easy veg recipe, easy non veg recipe"
-)
+    if os.path.exists(html_path):
+        with open(html_path, "r", encoding="utf-8") as f:
+            soup = BeautifulSoup(f, "html.parser")
 
-set_meta_property(soup, "og:title", "Chicken Recipe")
+        front_matter = get_meta_tags(md_path)
+        if front_matter:
+            # for each require meta tags
+            for mt in metas:
+                mt_val = front_matter.get(mt, None)
+                if mt_val:
+                    set_meta(soup, mt, mt_val)
+            # for each og meta tags
+            for og in meta_ogs:
+                og_val = front_matter.get(og, None)
+                if og_val:
+                    set_meta_property(soup, og, og_val)
+        # beautify the html
+        html = soup.prettify(formatter="html")
+        with open(html_path, "w", encoding="utf-8") as f:
+            f.write(html)
 
-# Beautify
-html = soup.prettify(formatter="html")
+        print(f"Finished updating: {html_path}")
 
-# Save the final output
-with open(html_file, "w", encoding="utf-8") as f:
-    f.write(html)
-
+# End
